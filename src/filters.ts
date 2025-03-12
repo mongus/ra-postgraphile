094c9f0f7852a82ced435a46b87fd62a1034d499
Aaron Porter <aaron@mongus.com>
2025-03-12 16:16:32 -0700

    Revert "trying to get case insensitive filters"
    This reverts commit ba577d1c125b9b72c52d6b3c8c10326b46b94319.

--- src/filters.ts
+++ src/filters.ts
@@ -47,7 +47,7 @@ export const mapFilterType = (
     let operator: Operator = 'equalTo'
     // string uses includes as the default operator for historical reasons
     if (typeName === 'string') {
-      operator = 'includesInsensitive'
+      operator = 'includes'
     }
 
     // a type of FullText uses matches as the default operator for historical reasons
